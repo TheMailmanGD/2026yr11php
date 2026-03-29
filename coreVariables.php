f13eebef6a5bd5aac59d5364dee0dbb865c532f1
themailmangd <themailmangd@gmail.com>
2026-03-30 09:35:27 +1100

    echo

--- coreVariables.php
+++ coreVariables.php
@@ -3,7 +3,13 @@ include_once "indexHeader.php";
 include_once "indexMenubar.php";
 include_once "container_start.php";
 ?>
+
 <h1>Variables</h1>
+<!-- how to write html inside php -->
+ <?php
+ echo ("<h3>this is how you write HTML inside PHP tag</h3>");
+ ?>
+
 <?php
 include_once "container_end.php";
 include_once "indexFooter.php";
