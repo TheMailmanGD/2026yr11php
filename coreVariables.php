2ebeca978b374c951a280c8111524da0a88da241
themailmangd <themailmangd@gmail.com>
2026-03-30 18:27:31 +1100

    variables

--- coreVariables.php
+++ coreVariables.php
@@ -8,6 +8,10 @@ include_once "container_start.php";
 <!-- how to write html inside php -->
  <?php
  echo ("<h3>this is how you write HTML inside PHP tag</h3>");
+ $hello = "Hello World";
+ print("$hello"); //both using quotes and not using quotes work
+ $x = 5;
+ echo ("<br>The square of $x is" . $x**);
  ?>
 
 <?php
