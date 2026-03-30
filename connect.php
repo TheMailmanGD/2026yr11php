fce53eb8193477e8bd9897ff27b6fbba57f87ab3
themailmangd <themailmangd@gmail.com>
2026-03-31 10:41:35 +1100

    connect

--- connect.php
+++ connect.php
@@ -7,10 +7,12 @@ $pass = '';
 // not neccessary
 date_default_timezone_set('Australia/Melbourne');
 
+$conn = new mysqli($host, $user, $pass, $db);
 //connection error handling
 if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 } else {
     echo ("connection successful. ");
 }
+$conn->close();
 ?>
--- crudRead.php
+++ crudRead.php
@@ -2,8 +2,19 @@
 include_once "indexHeader.php";
 ?>
 <h1>CRUD Read</h1>
+<?php
+$query1 = "SELECT * FROM users WHERE status = '1'";
+//echo('<br>' . $query1 . '<br>');
+$conn = new mysqli($host, $user, $pass, $db);
+$result = mysqli_query($conn, $query1);
+$conn->close();
 
+//var_dump($result);
 
+if ($result){
+
+}
+?>
 <?php
 include_once "indexFooter.php";
 ?>
