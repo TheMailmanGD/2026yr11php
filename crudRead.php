2a7193c5621462c3e674ad896013a2df08f3366e
themailmangd <themailmangd@gmail.com>
2026-03-31 10:43:06 +1100

    finsihed connection

--- crudRead.php
+++ crudRead.php
@@ -11,9 +11,36 @@ $conn->close();
 
 //var_dump($result);
 
-if ($result){
-
+if($result){ // querry runs
+    if(mysqli_num_rows($result) > 0){
+        $output = '<table class="table">';
+        $output .=  '<tr>
+                            <th>ID</th>
+                            <th>Code</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                        </tr>';
+        while($row = mysqli_fetch_array($result)){
+            $output .=  '<tr>
+                            <td>'. $row["id"] .'</td>
+                            <td>'. $row["code"] .'</td>
+                            <td>'. $row["first_name"] .'</td>
+                            <td>'. $row["last_name"] .'</td>
+                            <td>'. $row["email"] .'</td>
+                            <td>'. $row["status"] .'</td>
+                        </tr>';
+        }
+    } else {
+        echo ("No Records found");
+    }
+}else {
+    echo ("Query returned FALSE");
 }
+$output .=  "</table>";
+echo ($output);
+
 ?>
 <?php
 include_once "indexFooter.php";
