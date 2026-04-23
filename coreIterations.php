1643533739454b81d7b5d00009c234961b9ac5e7
themailmangd <themailmangd@gmail.com>
2026-04-23 10:40:18 +1000

    iterations

--- coreIterations.php
+++ coreIterations.php
@@ -5,10 +5,30 @@ include_once "indexHeader.php";
 <h1>Iterations</h1>
 
 <!-- for loop -->
-
+<?php
+//definition -> condition -> increment
+for ($i = 0; $i <= 10; $i++) {
+  echo ($x);
+}
+?>
 <!-- while do loop -->
+<?php
+$i = 1;
+while ($i <= 10) {
+    $i++; 
+    echo i;
+}
+?>
 
 <!-- do while loop -->
+<?php
+$i = 1;
+
+do {
+  echo $i;
+  $i++;
+} while ($i < 10);
+?>
 <?php
 include_once "indexFooter.php";
 ?>
